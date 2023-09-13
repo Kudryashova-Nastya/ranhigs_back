d117f4e6bf82101084848c32c794be7753450254
zakhep66 <sur.mikhael@gmail.com>
2023-09-13 16:14:11 +0300

    add endpoint list for team add simplejwt for token auth

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from app.views import TeamViewSet
 
+router = DefaultRouter()
+router.register(r'', TeamViewSet, basename='team')
 
 urlpatterns = [
-
+    path('team', include(router.urls)),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from app.models import Team
+from app.serializers import TeamSerializer
+
+
+class TeamViewSet(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    permission_classes = [IsAuthenticated, ]
